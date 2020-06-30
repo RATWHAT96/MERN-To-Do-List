a4363e907d1591c92361610a4440de3a896d98ac
Rahul Ratwatte <racr20@bath.ac.uk>
2020-06-30 18:30:35 +0100

    changed redux tool store

--- client/src/store.js
+++ client/src/store.js
@@ -6,10 +6,17 @@ const intitalState = {};
 
 const middleware = [thunk];
 
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION__() || compose;
+const store = createStore(reducer, intitalState, composeEnhancers(applyMiddleware(...middleware)));
+
+/*
 const store = createStore(rootReducer, intitalState, compose(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
+*/
+
 
 export default store;
 
