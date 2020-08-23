34a4465077a9a229054adae1861f5d6b964418a4
Rahul Ratwatte <racr20@bath.ac.uk>
2020-08-23 13:45:56 +0100

    Readded keys.js

--- client/src/store.js
+++ client/src/store.js
@@ -14,3 +14,4 @@ const store = createStore(rootReducer, intitalState, compose(
 
 export default store;
 
+
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+//Go to mondodb atlas online, login to clusters, click connect, selet connect using application and copy url and add in username and password. 
+module.exports = {
+    mongoURI: 'mongodb+srv://ratwhat123:zVQTH7t7cWb0VSjK@cluster0.k1joh.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority'
+} 
